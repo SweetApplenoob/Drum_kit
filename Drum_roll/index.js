f9450b95b04681e45d1c5111d2ef19ae35895251
SweetApplenoob <85312241+SweetApplenoob@users.noreply.github.com>
2022-08-11 23:25:48 +0530

    initial commit

---
+++ Drum_roll/index.js
@@ -0,0 +1,78 @@
+// We add event listener to all the buttons having class drum
+//when someone click the button with mouse
+
+var drum = document.querySelectorAll(".drum");
+for(var i = 0; i<drum.length; i++){
+    drum[i].addEventListener("click", function (){
+        
+        var clickedButton = this.innerHTML;
+        makeSound(clickedButton);
+        makeAnimation(clickedButton);
+
+    });
+    
+}
+
+//when KeyBoard button got pressed
+
+document.addEventListener("keydown" , function(event){
+    makeSound(event.key);
+    makeAnimation(event.key);
+});
+
+
+function makeSound(key){
+    switch (key) {
+        case "w":
+            var audio = new Audio('sounds/crash.mp3');
+            audio.play();
+    
+            break;
+        case "a":
+            var audio = new Audio('sounds/kick-bass.mp3');
+            audio.play();
+    
+            break;
+        case "s":
+            var audio = new Audio('sounds/snare.mp3');
+            audio.play();
+    
+            break;
+        case "d":
+            var audio = new Audio('sounds/tom-1.mp3');
+            audio.play();
+    
+            break;
+        case "j":
+            var audio = new Audio('sounds/tom-2.mp3');
+            audio.play();
+    
+            break;
+        case "k":
+            var audio = new Audio('sounds/tom-3.mp3');
+            audio.play();
+    
+            break;
+        case "l":
+            var audio = new Audio('sounds/tom-4.mp3');
+            audio.play();
+    
+            break;
+    
+        default:
+            break;
+    }
+   
+}
+
+
+
+function makeAnimation(currentKey){
+    var activeButton = document.querySelector("." + currentKey);
+    
+    activeButton.classList.add("pressed");
+    
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    },100);
+}
